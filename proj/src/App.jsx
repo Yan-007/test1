bccf6431cd7a38b3762a695638a23f68341229f6
Yan-007 <yansanzharov@gmail.com>
2024-02-12 15:51:03 +0400

    123

--- proj/src/App.jsx
+++ proj/src/App.jsx
@@ -1,14 +1,44 @@
 import React, {useState} from "react";
+import "./App.css";
 
 function App() {
-  const [clicks, setClicks] = useState(0);
+  const [clicks1, setClicks1] = useState(0);
+  const [clicks2, setClicks2] = useState(0);
   return (
     <div className="App">
-      <button onClick={()=>{setClicks(clicks + 1)}}>+</button>
-      <button onClick={()=>{setClicks(clicks - 1)}}>-</button>
-      <p>
-        {clicks}
-      </p>
+       <div class="chat-notification">
+      <div class="chat-notification-logo-wrapper">
+        <img class="chat-notification-logo" src="/img/logo.svg" alt="ChitChat Logo" />
+      </div>
+      <div class="chat-notification-content">
+        <h4 class="chat-notification-title">ChitChat</h4>
+        <p class="chat-notification-message">У вас новое сообщение!</p>
+      </div>
+    </div>
+      <div>
+      <div>
+          <button className="test border-2" onClick={()=>{setClicks1(clicks1 + 1)}}>+</button>
+          <button className="test border-2" onClick={()=>{setClicks1(clicks1 - 1)}}>-</button>
+          <button className="test border-2" onClick={()=>{setClicks1(0)}}>0</button>
+          <button className="test border-2" onClick={()=>{setClicks1(clicks1 * 10)}}>x10</button>
+          <button className="test border-2" onClick={()=>{setClicks1(clicks1 / 10)}}>/10</button>
+          <p>
+            {clicks1}
+          </p>
+        </div>
+        <div>
+          <button onClick={()=>{setClicks2(clicks2 + 1)}}>+</button>
+          <button onClick={()=>{setClicks2(clicks2 - 1)}}>-</button>
+          <button onClick={()=>{setClicks2(0)}}>0</button>
+          <button onClick={()=>{setClicks2(clicks2 * 10)}}>x10</button>
+          <button onClick={()=>{setClicks2(clicks2 / 10)}}>/10</button>
+          <p>
+            {clicks2}
+          </p>
+        </div>
+      </div>
+      
+      
     </div>
   );
 }
